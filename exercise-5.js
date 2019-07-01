d546803d6ddbfbf2054590dc98e377fa2190f343
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2019-07-01 15:29:36 +0700

    beautify

--- exercise-5.js
+++ exercise-5.js
@@ -26,7 +26,7 @@ var exampleFirstWord = word[0] + word[1] + word[2];
 var secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]; // do your own!
 var thirdWord = word[15] + word[16]; // do your own!
 var fourthWord = word[18] + word[19]; // do your own!
-var fifthWord= word[21] + word[22] + word[23] + word[24]; // do your own!
+var fifthWord = word[21] + word[22] + word[23] + word[24]; // do your own!
 
 console.log('First Word: ' + exampleFirstWord);
 console.log('Second Word: ' + secondWord);
@@ -44,10 +44,10 @@ console.log("----------------------------");
 
 var word3 = 'wow JavaScript is so cool';
 var exampleFirstWord3 = word.substring(0, 3);
-var secondWord3 = word.substring(4,14); // do your own!
-var thirdWord3 = word.substring(15,17); // do your own!
-var fourthWord3 = word.substring(18,20); // do your own!
-var fifthWord3 = word.substring(21,25); // do your own!
+var secondWord3 = word.substring(4, 14); // do your own!
+var thirdWord3 = word.substring(15, 17); // do your own!
+var fourthWord3 = word.substring(18, 20); // do your own!
+var fifthWord3 = word.substring(21, 25); // do your own!
 
 console.log('First Word: ' + exampleFirstWord);
 console.log('Second Word: ' + secondWord3);
@@ -65,10 +65,10 @@ console.log("----------------------------");
 
 var word4 = 'wow JavaScript is so cool';
 var exampleFirstWord4 = word.substring(0, 3);
-var secondWord4 = word.substring(4,14);  // do your own!
-var thirdWord4 = word.substring(15,17); // do your own!
-var fourthWord4 = word.substring(18,20); // do your own!
-var fifthWord4 = word.substring(21,25); // do your own!
+var secondWord4 = word.substring(4, 14); // do your own!
+var thirdWord4 = word.substring(15, 17); // do your own!
+var fourthWord4 = word.substring(18, 20); // do your own!
+var fifthWord4 = word.substring(21, 25); // do your own!
 
 var firstWordLength = exampleFirstWord4.length;
 var secondWordLength = secondWord4.length;
